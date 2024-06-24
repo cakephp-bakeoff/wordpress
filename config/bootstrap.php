ad72d1306cbf347f605b352ef87df71b69c76cc9
cakephp-bakeoff <cakephp-bakeoff@outlook.com>
2024-06-24 11:05:53 +0000

    Use first configured blog as default

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -2,3 +2,8 @@
 
 // Load configuration from APP/config/Bakeoff/Wordpress.php
 \Cake\Core\Configure::load($this->getName(), 'default');
+
+// Pick the first configured blog to be the default blog
+$blogList = \Cake\Core\Configure::read($this->getName().'.blogList');
+$blogSymbol = array_keys($blogList)[0];
+\Cake\Core\Configure::write($this->getName().'.defaultBlog', $blogSymbol);
