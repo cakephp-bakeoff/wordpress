ba17329af62143b0fd7d76de73b69af3af272730
cakephp-bakeoff <cakephp-bakeoff@outlook.com>
2024-07-01 17:36:25 +0000

    Create models for Categories and PostTags

--- src/Model/Table/WordpressAbstract/AbstractPostsTable.php
+++ src/Model/Table/WordpressAbstract/AbstractPostsTable.php
@@ -5,6 +5,24 @@ namespace Bakeoff\Wordpress\Model\Table\WordpressAbstract;
 abstract class AbstractPostsTable extends \Bakeoff\Wordpress\Model\Table\PluginTable
 {
 
+    public function initialize(array $config): void
+    {
+        parent::initialize($config);
+
+        $this->belongsToMany('Categories', [
+            'className' => 'Bakeoff/Wordpress.Categories',
+            'through' => 'Bakeoff/Wordpress.TermRelationships',
+            'foreignKey' => 'object_id',
+            'targetForeignKey' => 'term_taxonomy_id',
+        ]);
+        $this->belongsToMany('PostTags', [
+            'className' => 'Bakeoff/Wordpress.PostTags',
+            'through' => 'Bakeoff/Wordpress.TermRelationships',
+            'foreignKey' => 'object_id',
+            'targetForeignKey' => 'term_taxonomy_id',
+        ]);
+    }
+
     public function findPosts($type = 'all', $options = [])
     {
         $defaults = [
@@ -15,7 +33,7 @@ abstract class AbstractPostsTable extends \Bakeoff\Wordpress\Model\Table\PluginT
             'limit' => 5,
         ];
         $options += $defaults;
-        return $this->find($type, $options);
+        return $this->find($type, $options)->contain(['Categories', 'PostTags']);
     }
 
     public function findPublishedPosts($type = 'all', $options = [])
--- src/Model/Table/WordpressAbstract/AbstractTermTaxonomyTable.php
+++ src/Model/Table/WordpressAbstract/AbstractTermTaxonomyTable.php
@@ -5,6 +5,16 @@ namespace Bakeoff\Wordpress\Model\Table\WordpressAbstract;
 abstract class AbstractTermTaxonomyTable extends \Bakeoff\Wordpress\Model\Table\PluginTable
 {
 
+    public function initialize(array $config): void
+    {
+        parent::initialize($config);
+
+        $this->belongsTo('Terms', [
+            'foreignKey' => 'term_id',
+            'joinType' => 'INNER',
+            'className' => 'Bakeoff/Wordpress.Terms',
+        ]);
+    }
 
 
 }
