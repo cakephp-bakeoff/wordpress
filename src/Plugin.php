488e5ec28cfd5e0e78be0956018e3923d9eecd32
cakephp-bakeoff <cakephp-bakeoff@outlook.com>
2024-11-18 18:22:39 +0000

    Remove unnecessary bootstrap() declaration

--- src/Plugin.php
+++ src/Plugin.php
@@ -3,14 +3,8 @@
 namespace Bakeoff\Wordpress;
 
 use Cake\Core\BasePlugin;
-use Cake\Core\PluginApplicationInterface;
 
 class Plugin extends BasePlugin
 {
-
-    public function bootstrap(PluginApplicationInterface $app): void
-    {
-        parent::bootstrap($app);
-    }
 
 }
